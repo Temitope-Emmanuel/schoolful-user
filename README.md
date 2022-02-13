a71c16c9d4f12849cb7b012bbdfb8b7658eadbed
Temitope Emmanuel OJo <temitopeojo0@gmail.com>
2022-02-13 13:35:23 +0100

    updated readme again

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 
+# Welcome to Schoolful-user
+
+This is a project (user part) that comes with awesome functionality, built with react and using chakra-ui with material-ui (a bit overkill), there is also a livestreaming service althought you'll have to setup an RTMP server. You can reach out to me if you need any extra help, the codebase is about 2 years old so something might need minor changes but it should still be good to go. Happy hacking :) 
+
+https://www.loom.com/share/9bfd414ce0cb4cea90c9512e4c41b05d
+## This guide should help you get started
+
+**Step 1:** Clone the repository
+
+**Step 2:** Install dependencies by running `yarn`
+
+**Step 3:** Copy the env file and write your own env secret.
+
+https://www.loom.com/share/76ae094687714d53a5575f8b2cfbcf23
+
+## Features
+
+- Auth system
+- Fullcalendar integration with googleCalendar API
+- Google Streaming API with youtube
+- Payment system with Paystack
+- State management using (redux)
+- Carousel
+- Custom built calendar
+
+#### to use the extra feature you can add the token to the env file and uncomment out the code for running them. You can contact admin @ temitopeojo0@gmail.com
--- src/core/services/mock/mockService/activity.mock.ts
+++ src/core/services/mock/mockService/activity.mock.ts
@@ -22,53 +22,8 @@ export const activityMock = (mock: MockAdapter) => {
     })
 }
 
-
-// export const createActivity = async (arg:IActivity):Promise<IResponse<IActivity>> => {
-//     const url = `${baseUrl}/Activity/CreateActivity`
-//     try{
-//         const config:AxiosRequestConfig = {headers:{"Content-Type":"application/json-patch+json"}}
-//         const response = await axios.post(url,arg,config)
-//         return response.data
-//     }catch(err){
-//         throw err
-//     }
-// }
-
-
 // export const getChurchActivity = async (churchId:string):Promise<IResponse<IActivity[]>> => {
 //     const url = `${baseUrl}/Activity/GetChurchActivity/${churchId}`
-//     try{
-//         const config:AxiosRequestConfig = {headers:{Accept:"text/plain"}}
-//         const response = await axios.get(url,config)
-//         return response.data
-//     }catch(err){
-//         throw err
-//     }
-// }
-
-// export const updateActivity = async (arg:IActivity):Promise<IResponse<null>> => {
-//     const url = `${baseUrl}/Activity/UpdateActivity`
-//     try{
-//         const response = await axios.put(url,arg)
-//         return response.data
-//     }catch(err){
-//         throw err
-//     }
-// }
-
-// export const updateEvent = async (arg:IEvent):Promise<IResponse<IEvent>> => {
-//     const url = `${baseUrl}/Activity/UpdateEvents`
-//     try{
-//         const config:AxiosRequestConfig = {headers:{"Content-Type":"application/json-patch+json"}}
-//         const response = await axios.put(url,arg,config)
-//         return response.data
-//     }catch(err){
-//         throw err
-//     }
-// }
-
-// export const getChurchEvent = async (churchId:string):Promise<IResponse<IEvent[]>> => {
-//     const url = `${baseUrl}/Activity/GetChurchEvent?churchId=${churchId}`
 //     try{
 //         const config:AxiosRequestConfig = {headers:{Accept:"text/plain"}}
 //         const response = await axios.get(url,config)
